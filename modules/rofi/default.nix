327f3644f418d9840823213aacb3421c700c7497
vuongprogk <vuong753333@gmail.com>
2025-05-02 11:54:37 +0700

    refactor: move to rofi instead of wofi

---
+++ modules/rofi/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+let cfg = config.modules.rofi;
+
+in {
+    options.modules.rofi = { enable = mkEnableOption "rofi"; };
+    config = mkIf cfg.enable {
+      programs.rofi = {
+        enable = true;
+        package = pkgs.rofi-wayland;
+      };
+      home.file.".config/rofi/config.rasi".source = ./config.rasi;
+    };
+}
