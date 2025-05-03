302e06c4eebac470d48a28616f6fc71ad68ae166
vuongprogk <vuong753333@gmail.com>
2025-05-03 21:53:18 +0700

    fix: update color and opacity

--- modules/foot/default.nix
+++ modules/foot/default.nix
@@ -8,35 +8,48 @@ in {
     config = mkIf cfg.enable {
         programs.foot = {
             enable = true;
-            settings = {
-                main = {
-                    font = "JetBrainsMono NF:size=12:weight=semibold";
-                    pad = "12x12";
-                };
-                colors = {
-                    foreground = "d9e0ee";
-                    background = "292a37";
-                    ## Normal/regular colors (color palette 0-7)
-                    regular0="303241";  # black
-                    regular1="ec6a88";
-                    regular2="3fdaa4";
-                    regular3="efb993";
-                    regular4="3fc6de";
-                    regular5="b771dc";
-                    regular6="6be6e6";
-                    regular7="d9e0ee";
-
-                    bright0="393a4d"; # bright black
-                    bright1="e95678"; # bright red
-                    bright2="29d398";# bright green
-                    bright3="efb993";# bright yellow
-                    bright4="26bbd9";
-                    bright5="b072d1";# bright magenta
-                    bright6="59e3e3";# bright cyan
-                    bright7="d9e0ee";# bright white
-                    alpha=0.88;
-                };
-            };
         };
+        home.file.".config/foot/foot.ini".text = ''
+          [main]
+          font=JetBrainsMono NF:size=12:weight=semibold
+          pad=12x12
+          [cursor]
+          color=11111b f5e0dc
+
+          [colors]
+          foreground=cdd6f4
+          background=1e1e2e
+
+          regular0=45475a
+          regular1=f38ba8
+          regular2=a6e3a1
+          regular3=f9e2af
+          regular4=89b4fa
+          regular5=f5c2e7
+          regular6=94e2d5
+          regular7=bac2de
+
+          bright0=585b70
+          bright1=f38ba8
+          bright2=a6e3a1
+          bright3=f9e2af
+          bright4=89b4fa
+          bright5=f5c2e7
+          bright6=94e2d5
+          bright7=a6adc8
+
+          16=fab387
+          17=f5e0dc
+
+          selection-foreground=cdd6f4
+          selection-background=414356
+
+          search-box-no-match=11111b f38ba8
+          search-box-match=cdd6f4 313244
+
+          jump-labels=11111b fab387
+          urls=89b4fa
+          alpha=0.9
+        '';
     };
 }
